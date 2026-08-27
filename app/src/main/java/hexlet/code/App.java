16c5992dfa425ec501dc52472e046799af5d639f
EgorAL15 <alexanderovesar56789@gmail.com>
2026-08-27 10:50:44 +0300

    Add CLI arguments and format option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "gendiff",
@@ -10,6 +12,25 @@ import picocli.CommandLine.Command;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Runnable {
+
+    @Parameters(
+            index = "0",
+            description = "path to first file"
+    )
+    private String filepath1;
+
+    @Parameters(
+            index = "1",
+            description = "path to second file"
+    )
+    private String filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish"
+    )
+    private String format;
 
     @Override
     public void run() {
